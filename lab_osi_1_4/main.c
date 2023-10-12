515118f3e7fb56ea78936a35a0d0ac64ddc1d809
ykwais <140707281+ykwais@users.noreply.github.com>
2023-10-12 16:24:17 +0300

    lab osi 1.4

--- lab_osi_1_4/main.c
+++ lab_osi_1_4/main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "enums.h"
 #include "funcs.c"
 
@@ -9,10 +7,14 @@ int main(int argc, char **argv) {
     int answer = 0;
     char result[4] = {0, 0, 0, 0};
     int counter = 0;
-    int mask = 0;
+
     file_troubles stat;
+    memory mem_status;
+    valid_mask mask_status;
+    char* mask;
 
     switch (analysis(argc, argv)) {
+
         case isc_too_args:
             printf("You've inputted too many/little arguments");
             break;
@@ -21,22 +23,43 @@ int main(int argc, char **argv) {
             break;
         case isc_xor8:
             xor_8(argv[2], &answer, &stat);
+            if(stat == ofp_bad_open)
+            {
+                printf("Bad opening the file!");
+                return 0;
+            }
             printf("answer for xor_2 = %d\n", answer);
             break;
         case isc_xor32:
             xor_32(argv[2], result, &stat);
+            if(stat == ofp_bad_open)
+            {
+                printf("Bad opening the file!");
+                return 0;
+            }
             printf("answer for xor_32 : \n");
-            for (int i = 0; i < 4; ++i) {
+            for (int i = 0; i < 4; ++i)
+            {
                 printf("%d ", result[i]);
             }
             break;
         case isc_mask:
-            mask = strtol(argv[3], NULL, 16);
-            if (errno == EINVAL) {
-                printf("wrong <hex>");
+            mask = get_mask(argv[3], &mem_status, &mask_status);
+
+            if(mem_status == mt_memory_problem){
+                printf("There are problems with a memory allocation!");
                 return 0;
             }
-            mask_func(argv[2], &counter, &mask, &stat);
+            mask_func(argv[2], &counter, mask, &stat);
+            if(stat == ofp_bad_open)
+            {
+                printf("Bad opening the file!");
+                return 0;
+            }
+            if(mask_status == vvm_invalid)
+            {
+                printf("\nall invalid mask values have been replaced with 0\n");
+            }
             printf("\nresult of mask : %d\n", counter);
             break;
     }
